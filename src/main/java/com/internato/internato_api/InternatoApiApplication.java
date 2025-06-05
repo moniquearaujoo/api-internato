10d5c68c0882d3fb619d8aecb8ba066a0461ab11
Monique Araujo <moniquearjm@gmail.com>
2025-06-04 23:29:53 -0300

    criando projeto, primeiro commit

---
+++ src/main/java/com/internato/internato_api/InternatoApiApplication.java
@@ -0,0 +1,13 @@
+package com.internato.internato_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternatoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternatoApiApplication.class, args);
+	}
+
+}
