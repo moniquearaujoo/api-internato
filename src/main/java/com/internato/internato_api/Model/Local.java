f19b019b16362d3459ee4d87244ffc341e09f17a
Racc3 <rudah87.2@gmail.com>
2025-06-05 21:48:24 -0300

    Entidades Local e Preceptor

--- src/main/java/com/internato/internato_api/Model/Local.java
+++ src/main/java/com/internato/internato_api/Model/Local.java
@@ -1,12 +1,18 @@
 package com.internato.internato_api.Model;
 
 import jakarta.persistence.*;
-
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data  // gera getters, setters, equals, hashcode, toString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Local {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
---
+++ src/main/java/com/internato/internato_api/Repository/PreceptorRepository.java
@@ -0,0 +1,7 @@
+package com.internato.internato_api.Repository;
+
+import com.internato.internato_api.Model.Preceptor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PreceptorRepository extends JpaRepository<Preceptor, Long> {
+}
